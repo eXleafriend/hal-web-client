248d7cac9555f46655577e9e247a62cdf3522028
leafriend <leafriend@gmail.com>
2024-06-08 01:53:25 +0900

    add baseUrl

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,8 +6,36 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [baseUrl, _setBaseUrl] = useState(localStorage.getItem("baseUrl"));
+  const [baseUrlEnabled, setBaseUrlEnabled] = useState(false);
+
+  function setBaseUrl(_baseUrl: string): void {
+    localStorage.setItem("baseUrl", _baseUrl);
+    _setBaseUrl(_baseUrl);
+  }
+
+  useEffect(() => {
+    if (baseUrl === null) {
+      setBaseUrlEnabled(false);
+      return;
+    }
+    fetch(baseUrl).then(response => {
+      if (100 <= response.status && response.status < 400) {
+        setBaseUrlEnabled(true);
+      } else {
+        setBaseUrlEnabled(false);
+      }
+    });
+  }, [baseUrl]);
+
   return (
     <>
+      <div>
+        <input type="text" placeholder="http://localhost:8080" value={baseUrl || ""} onChange={e => setBaseUrl(e.target.value)} />
+      </div>
+      <form>
+        <button disabled={!baseUrlEnabled}>Call</button>
+      </form>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
